88bfeaf6dc4bf599b9bd14c4c433361157a3f204
16121a1228 <gjyothsna1228@gmail.com>
2022-01-12 11:49:40 +0530

    commit

---
+++ stage3/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import {  Link } from 'react-router-dom';
+
+
+const Home = () => {
+    return (
+        <div className="quizboard invitation">
+            <h1>Quiz App</h1>
+            <Link to={"/quiz"}><button className="play">Play</button></Link>
+        </div>
+    )
+}
+export default Home
